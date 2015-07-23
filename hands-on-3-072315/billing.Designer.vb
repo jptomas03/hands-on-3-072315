a4b43c343316847b461ff6b15c084173b6d45a56
jptomas03 <jptomas03@yahoo.com>
2015-07-23 18:27:43 +0800

    3rd commit

--- hands-on-3-072315/billing.Designer.vb
+++ hands-on-3-072315/billing.Designer.vb
@@ -22,7 +22,7 @@ Partial Class billing
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComputeButton = New System.Windows.Forms.Button()
         Me.NameLabel = New System.Windows.Forms.Label()
         Me.NameTB = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -36,21 +36,26 @@ Partial Class billing
         Me.AmountDueLabel = New System.Windows.Forms.Label()
         Me.RegistrationFeeTB = New System.Windows.Forms.TextBox()
         Me.RegistrationFeeLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'ComputeButton
         '
-        Me.Button1.Location = New System.Drawing.Point(100, 226)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(244, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Compute"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.ComputeButton.Location = New System.Drawing.Point(475, 150)
+        Me.ComputeButton.Name = "ComputeButton"
+        Me.ComputeButton.Size = New System.Drawing.Size(244, 23)
+        Me.ComputeButton.TabIndex = 0
+        Me.ComputeButton.Text = " Compute"
+        Me.ComputeButton.UseVisualStyleBackColor = True
         '
         'NameLabel
         '
         Me.NameLabel.AutoSize = True
-        Me.NameLabel.Location = New System.Drawing.Point(97, 72)
+        Me.NameLabel.Location = New System.Drawing.Point(109, 48)
         Me.NameLabel.Name = "NameLabel"
         Me.NameLabel.Size = New System.Drawing.Size(35, 13)
         Me.NameLabel.TabIndex = 1
@@ -58,15 +63,15 @@ Partial Class billing
         '
         'NameTB
         '
-        Me.NameTB.Location = New System.Drawing.Point(244, 65)
+        Me.NameTB.Location = New System.Drawing.Point(81, 74)
         Me.NameTB.Name = "NameTB"
-        Me.NameTB.Size = New System.Drawing.Size(100, 20)
+        Me.NameTB.Size = New System.Drawing.Size(143, 20)
         Me.NameTB.TabIndex = 2
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(154, 9)
+        Me.Label2.Location = New System.Drawing.Point(335, 9)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(150, 13)
         Me.Label2.TabIndex = 3
@@ -74,15 +79,15 @@ Partial Class billing
         '
         'AddressTB
         '
-        Me.AddressTB.Location = New System.Drawing.Point(244, 91)
+        Me.AddressTB.Location = New System.Drawing.Point(230, 74)
         Me.AddressTB.Name = "AddressTB"
-        Me.AddressTB.Size = New System.Drawing.Size(100, 20)
+        Me.AddressTB.Size = New System.Drawing.Size(212, 20)
         Me.AddressTB.TabIndex = 5
         '
         'AddressLabel
         '
         Me.AddressLabel.AutoSize = True
-        Me.AddressLabel.Location = New System.Drawing.Point(97, 98)
+        Me.AddressLabel.Location = New System.Drawing.Point(317, 48)
         Me.AddressLabel.Name = "AddressLabel"
         Me.AddressLabel.Size = New System.Drawing.Size(45, 13)
         Me.AddressLabel.TabIndex = 4
@@ -90,7 +95,7 @@ Partial Class billing
         '
         'TrainingFeeTB
         '
-        Me.TrainingFeeTB.Location = New System.Drawing.Point(244, 143)
+        Me.TrainingFeeTB.Location = New System.Drawing.Point(619, 67)
         Me.TrainingFeeTB.Name = "TrainingFeeTB"
         Me.TrainingFeeTB.ReadOnly = True
         Me.TrainingFeeTB.Size = New System.Drawing.Size(100, 20)
@@ -100,7 +105,7 @@ Partial Class billing
         'TrainingFeeLabel
         '
         Me.TrainingFeeLabel.AutoSize = True
-        Me.TrainingFeeLabel.Location = New System.Drawing.Point(97, 150)
+        Me.TrainingFeeLabel.Location = New System.Drawing.Point(472, 74)
         Me.TrainingFeeLabel.Name = "TrainingFeeLabel"
         Me.TrainingFeeLabel.Size = New System.Drawing.Size(66, 13)
         Me.TrainingFeeLabel.TabIndex = 8
@@ -108,7 +113,7 @@ Partial Class billing
         '
         'NoOfParticipantsTB
         '
-        Me.NoOfParticipantsTB.Location = New System.Drawing.Point(244, 117)
+        Me.NoOfParticipantsTB.Location = New System.Drawing.Point(619, 41)
         Me.NoOfParticipantsTB.Name = "NoOfParticipantsTB"
         Me.NoOfParticipantsTB.Size = New System.Drawing.Size(100, 20)
         Me.NoOfParticipantsTB.TabIndex = 7
@@ -116,7 +121,7 @@ Partial Class billing
         'NumberOfPariticipantsLabel
         '
         Me.NumberOfPariticipantsLabel.AutoSize = True
-        Me.NumberOfPariticipantsLabel.Location = New System.Drawing.Point(97, 124)
+        Me.NumberOfPariticipantsLabel.Location = New System.Drawing.Point(472, 48)
         Me.NumberOfPariticipantsLabel.Name = "NumberOfPariticipantsLabel"
         Me.NumberOfPariticipantsLabel.Size = New System.Drawing.Size(114, 13)
         Me.NumberOfPariticipantsLabel.TabIndex = 6
@@ -124,7 +129,7 @@ Partial Class billing
         '
         'AmountDueTB
         '
-        Me.AmountDueTB.Location = New System.Drawing.Point(244, 195)
+        Me.AmountDueTB.Location = New System.Drawing.Point(619, 123)
         Me.AmountDueTB.Name = "AmountDueTB"
         Me.AmountDueTB.ReadOnly = True
         Me.AmountDueTB.Size = New System.Drawing.Size(100, 20)
@@ -133,7 +138,7 @@ Partial Class billing
         'AmountDueLabel
         '
         Me.AmountDueLabel.AutoSize = True
-        Me.AmountDueLabel.Location = New System.Drawing.Point(97, 202)
+        Me.AmountDueLabel.Location = New System.Drawing.Point(472, 126)
         Me.AmountDueLabel.Name = "AmountDueLabel"
         Me.AmountDueLabel.Size = New System.Drawing.Size(66, 13)
         Me.AmountDueLabel.TabIndex = 12
@@ -141,7 +146,7 @@ Partial Class billing
         '
         'RegistrationFeeTB
         '
-        Me.RegistrationFeeTB.Location = New System.Drawing.Point(244, 169)
+        Me.RegistrationFeeTB.Location = New System.Drawing.Point(619, 93)
         Me.RegistrationFeeTB.Name = "RegistrationFeeTB"
         Me.RegistrationFeeTB.ReadOnly = True
         Me.RegistrationFeeTB.Size = New System.Drawing.Size(100, 20)
@@ -151,17 +156,60 @@ Partial Class billing
         'RegistrationFeeLabel
         '
         Me.RegistrationFeeLabel.AutoSize = True
-        Me.RegistrationFeeLabel.Location = New System.Drawing.Point(97, 176)
+        Me.RegistrationFeeLabel.Location = New System.Drawing.Point(472, 100)
         Me.RegistrationFeeLabel.Name = "RegistrationFeeLabel"
         Me.RegistrationFeeLabel.Size = New System.Drawing.Size(84, 13)
         Me.RegistrationFeeLabel.TabIndex = 10
         Me.RegistrationFeeLabel.Text = "Registration Fee"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 150)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = " add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(230, 100)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox1.TabIndex = 16
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(81, 100)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(143, 20)
+        Me.TextBox2.TabIndex = 15
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(230, 126)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox3.TabIndex = 18
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(81, 126)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(143, 20)
+        Me.TextBox4.TabIndex = 17
+        '
         'billing
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(453, 261)
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(840, 261)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.AmountDueTB)
         Me.Controls.Add(Me.AmountDueLabel)
         Me.Controls.Add(Me.RegistrationFeeTB)
@@ -175,14 +223,14 @@ Partial Class billing
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.NameTB)
         Me.Controls.Add(Me.NameLabel)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComputeButton)
         Me.Name = "billing"
         Me.Text = "Manny Managemet Consulting"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComputeButton As System.Windows.Forms.Button
     Friend WithEvents NameLabel As System.Windows.Forms.Label
     Friend WithEvents NameTB As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
@@ -196,5 +244,10 @@ Partial Class billing
     Friend WithEvents AmountDueLabel As System.Windows.Forms.Label
     Friend WithEvents RegistrationFeeTB As System.Windows.Forms.TextBox
     Friend WithEvents RegistrationFeeLabel As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
 
 End Class
--- hands-on-3-072315/billing.vb
+++ hands-on-3-072315/billing.vb
@@ -1,8 +1,17 @@
 ﻿Public Class billing
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ComputeButton.Click
         Dim product As Integer
+        
         product = RegistrationFeeTB.Text * NoOfParticipantsTB.Text
         AmountDueTB.Text = product + +TrainingFeeTB.Text
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim textbox1 As New TextBox
+        textbox1.Name = "Textbox1"
+        textbox1.Size = New Size(170, 20)
+        textbox1.Location = New Point(81, 152)
+        Me.Controls.Add(textbox1)
     End Sub
 End Class
